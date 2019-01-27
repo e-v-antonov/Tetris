887289c4bb22a390d30d7bb8f7b1909274b81975
евгений антонов <antonov.eugeny2012@yandex.ru>
2019-01-27 21:07:01 +0300

    Создан класс для рисования фигуры

--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -14,26 +14,6 @@ namespace Tetris
 {
     public partial class Form1 : Form
     {
-        string[] list_Multiplier  = {"00010203",   //список множителей
-                              "00011102",
-                              "00011112",
-                              "00011121",
-                              "00100102",
-                              "00100111",
-                              "00101112",
-                              "00101121",
-                              "00102011",
-                              "00102030",
-                              "10011112",
-                              "10011121",
-                              "10110102",
-                              "10110212",
-                              "10200111"};
-        int choice_Multiplier; //выбор множителя
-        string string_Multiplier;   //строка множителей
-        int x_Multiplier, y_Multiplier;   //множители координат х и у
-        char [] digit_Multiplier = new char [8]; //цифры множители
-
         public Form1()
         {
             InitializeComponent();
@@ -44,26 +24,10 @@ namespace Tetris
             Invalidate();
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        public void panel1_Paint(object sender, PaintEventArgs e)
         {
-            Random rnd = new Random();
-            choice_Multiplier = rnd.Next(0, 15);
-            string_Multiplier = list_Multiplier[choice_Multiplier];
-            digit_Multiplier = string_Multiplier.ToArray();            
-            
-            for (int i = 0; i < digit_Multiplier.Length; i++)
-            {                
-                if ((i == 0) ||(i == 2) || (i == 4) || (i == 6))
-                {
-                    x_Multiplier = Convert.ToInt32(digit_Multiplier[i].ToString());
-                    continue;                                      
-                }
-                else
-                {
-                    y_Multiplier = Convert.ToInt32(digit_Multiplier[i].ToString());
-                    e.Graphics.FillRectangle(Brushes.Red, x_Multiplier * 24, y_Multiplier * 24, 24, 24);
-                }
-            }
+            Figure figure = new Figure();
+            figure.Draw(sender, e);
         }
     }
 }
