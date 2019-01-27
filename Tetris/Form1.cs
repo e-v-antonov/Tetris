f613d400d66807d4d22fe5268e03eb42ae0b3a8b
евгений антонов <antonov.eugeny2012@yandex.ru>
2019-01-27 14:47:59 +0300

    Добавьте файлы проекта.

---
+++ Tetris/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+
+        int x, y;
+        int zapx, zapy;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            //Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            ////
+
+
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 52, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 78, 0, 24, 24);
+
+
+            //
+            //
+            //
+            //
+
+
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 26, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 52, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 78, 24, 24);
+
+
+            //
+            //
+
+
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 26, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 26, 24, 24);
+
+
+            ////////
+            //
+
+
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 52, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 26, 24, 24);
+
+
+            //
+            ////
+            //
+
+
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 26, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 52, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 26, 24, 24);
+
+
+            //
+            ////
+            //
+
+            //e.Graphics.FillRectangle(Brushes.Red, 0, 26, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 0, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 26, 24, 24);
+            //e.Graphics.FillRectangle(Brushes.Red, 26, 52, 24, 24);
+
+
+            //
+            //////
+
+            for (int i = 0; i < 4; i++)
+            {
+                Random rnd = new Random();
+                Random rnd1 = new Random();
+
+                x = rnd.Next(0, 3);
+                y = rnd.Next(0, 3);
+
+
+                //e.Graphics.DrawString(x.ToString(), Font, SystemBrushes.WindowText, i + 5, 0);
+                //panel1.Refresh();
+                e.Graphics.FillEllipse(Brushes.Red, new Rectangle(x * 24, x * 24, 24, 24));
+
+            }
+
+            //e.Graphics.FillEllipse(Brushes.Red, new Rectangle(0 * 24, 0 * 24, 24, 24));
+            //e.Graphics.FillEllipse(Brushes.Red, new Rectangle(1 * 24, 0 * 24, 24, 24));
+            //e.Graphics.FillEllipse(Brushes.Red, new Rectangle(2 * 24, 0 * 24, 24, 24));
+            //e.Graphics.FillEllipse(Brushes.Red, new Rectangle(3 * 24, 0 * 24, 24, 24));
+
+
+            //00010203
+            //00011102
+            //00011112
+            //00011121                        
+            //00100102
+            //00100111
+            //00101112            
+            //00101121
+            //00102011                      
+            //00102030
+
+            //10011112
+            //10011121
+            //10110102
+            //10110212            
+            //10200111
+
+
+        }
+    }
+}
---
+++ Tetris/Primer.cs
@@ -0,0 +1,245 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Text;
+//using System.Windows.Forms;
+//using System.Media;
+
+//namespace Tetris
+//{
+//    class Primer
+//    {
+//        const int MAXX = 10;    //кол-во кружков по горизонтали
+//        const int MAXY = 15;    //кол-во кружков по вертикали
+//        const int ps = 24;  //кол-во пикселей в ширине и длине кружка
+//        bool started = false;
+//        Brush[] bru = {Brushes.White, Brushes.Orange, Brushes.Blue, Brushes.Red, Brushes.Green, Brushes.Honeydew,
+//            Brushes.Violet, Brushes.Tomato, Brushes.SteelBlue, Brushes.PapayaWhip};
+
+//        int score;
+//        int[,] ar;
+//        Figure figure;
+//        Graphics gr;
+//        SoundPlayer simpleSound = new SoundPlayer("chimes.wav");
+
+//        private void draw(int x, int y, int clr)    //рисование кружка
+//        {
+//            if (clr == 0 || x < 0 || x >= MAXX || y < 0 || y >= MAXY) return;   //если 
+//            gr.FillEllipse(bru[clr], new Rectangle(2 + x * ps, 2 + y * ps, ps, ps));    //рисование одного кружка
+//        }
+
+//        private void draw_field()   //"рисование поля"
+//        {
+//            int i, j;
+//            for (i = 0; i < MAXX; ++i)  //пока i меньше кол-во кружков по горизонтали
+//                for (j = 0; j < MAXY; ++j)  //пока i меньше кол-во кружков по вертикали
+//                    draw(i, j, ar[i, j]);
+//        }
+
+//        private void draw_figure(Figure f)  //"рисование фигуры"
+//        {
+//            int i, j;
+//            for (i = 0; i < 4; ++i)
+//                for (j = 0; j < 4; ++j)
+//                    draw(f.x + i, f.y + j, f.pix[i, j]);
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            started = true;
+//            score = 0;
+//            ar = new int[MAXX, MAXY];
+//            upd_score();
+//            figure = null;
+//            timer1.Enabled = true;
+//        }
+
+//        private int get_ar(int x, int y)
+//        {
+//            if (x < 0 || x >= MAXX || y >= MAXY) return 1;
+//            if (y < 0) return 0;
+//            return ar[x, y];
+//        }
+
+//        private void set_ar(int x, int y, int clr)
+//        {
+//            if (x < 0 || x >= MAXX || y < 0 || y >= MAXY) return;
+//            ar[x, y] = clr;
+//        }
+
+//        private bool figure_ok()
+//        {
+//            int i, j;
+//            for (i = 0; i < 4; ++i)
+//                for (j = 0; j < 4; ++j)
+//                {
+//                    if (figure.pix[i, j] != 0 && get_ar(figure.x + i, figure.y + j) != 0)
+//                        return false;
+//                }
+//            return true;
+//        }
+
+//        private void apply_figure()
+//        {
+//            int i, j;
+//            for (i = 0; i < 4; ++i)
+//                for (j = 0; j < 4; ++j)
+//                    if (figure.pix[i, j] != 0) set_ar(figure.x + i, figure.y + j, figure.pix[i, j]);
+//            bool[] lines = new bool[MAXY];
+//            int goo = 0;
+//            for (i = 0; i < MAXY; ++i)
+//            {
+//                bool qq = true;
+//                for (j = 0; j < MAXX; ++j)
+//                {
+//                    if (ar[j, i] == 0)
+//                    {
+//                        qq = false;
+//                        break;
+//                    }
+//                }
+//                lines[i] = qq;
+//                if (qq) ++goo;
+//            }
+//            if (goo != 0)
+//            {
+//                int sc = (goo * (goo + 1) / 2) * 100;
+//                score += sc;
+//                upd_score();
+//                simpleSound.Play();
+//            }
+//            int[,] nar = new int[MAXX, MAXY];
+//            int ty = MAXY;
+//            for (i = MAXY - 1; i >= 0; --i)
+//            {
+//                if (lines[i]) continue;
+//                --ty;
+//                for (j = 0; j < MAXX; ++j)
+//                {
+//                    nar[j, ty] = ar[j, i];
+//                }
+//            }
+//            ar = nar;
+//        }
+
+//        private void figure_down()
+//        {
+//            ++figure.y;
+//            if (!figure_ok())
+//            {
+//                --figure.y;
+//                apply_figure();
+//                figure = null;
+//            }
+//        }
+
+//        private void timer1_Tick(object sender, EventArgs e)
+//        {
+//            if (figure == null)
+//            {
+//                figure = new Figure();
+//                if (!figure_ok())
+//                {
+//                    timer1.Enabled = false;
+//                    MessageBox.Show("Game Over!");
+//                }
+//            }
+//            figure_down();
+//            panel1.Refresh();
+//        }
+
+//        private void panel1_Paint(object sender, PaintEventArgs e)
+//        {
+//            if (!started) return;
+//            gr = e.Graphics;
+//            gr.DrawRectangle(Pens.LightBlue, new Rectangle(0, 0, ps * MAXX + 4, ps * MAXY + 4));
+//            gr.FillRectangle(Brushes.Black, new Rectangle(1, 1, ps * MAXX + 3, ps * MAXY + 3));
+//            draw_field();
+//            if (figure != null) draw_figure(figure);
+//        }
+
+//        private void Form1_KeyDown(object sender, KeyEventArgs e)
+//        {
+//            if (figure == null) return;
+//            if (e.KeyCode == Keys.NumPad4)
+//            {
+//                figure.x -= 1;
+//                if (!figure_ok()) figure.x += 1;
+//            }
+//            else if (e.KeyCode == Keys.NumPad6)
+//            {
+//                figure.x += 1;
+//                if (!figure_ok()) figure.x -= 1;
+//            }
+//            else if (e.KeyCode == Keys.NumPad2)
+//            {
+//                figure.y += 1;
+//                if (!figure_ok()) figure.y -= 1;
+//            }
+//            else if (e.KeyCode == Keys.NumPad8)
+//            {
+//                figure.rotate();
+//                if (!figure_ok())
+//                {
+//                    for (int i = 0; i < 3; ++i) figure.rotate();
+//                }
+//            }
+//            panel1.Refresh();
+//        }
+
+//        private void button1_KeyDown(object sender, KeyEventArgs e)
+//        {
+//            Form1_KeyDown(sender, e);
+//        }
+
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+//            panel1.enableDB();
+//        }
+//    }
+
+//    public class Figure
+//    {
+//        string[] nya = {"....1111........", "....111.1.......", ".....11..11.....", ".1...11...1.....",
+//            "....111..1......", "....111...1.....", "..1..11..1......"};
+//        public int x, y;
+//        public int clr;
+//        public int[,] pix;
+//        public Random rnd;
+//        public Figure()
+//        {
+//            x = 3; y = -2;
+//            rnd = new Random();
+//            clr = 1 + rnd.Next(9);
+//            int n = rnd.Next(nya.Length);
+//            fillpix(out pix, nya[n]);
+//            n = rnd.Next(4);
+//            for (int i = 0; i < n; ++i)
+//                rotate();
+//        }
+//        void fillpix(out int[,] pix, string cc)
+//        {
+//            pix = new int[4, 4];
+//            int i, j;
+//            for (i = 0; i < 4; ++i)
+//                for (j = 0; j < 4; ++j)
+//                {
+//                    if (cc[j * 4 + i] == '1') pix[i, j] = clr;
+//                    else pix[i, j] = 0;
+//                }
+//        }
+//        public void rotate()
+//        {
+//            int[,] p2 = new int[4, 4];
+//            int i, j;
+//            for (i = 0; i < 4; ++i)
+//                for (j = 0; j < 4; ++j)
+//                {
+//                    p2[3 - j, i] = pix[i, j];
+//                }
+//            pix = p2;
+//        }
+//    }
+//}
